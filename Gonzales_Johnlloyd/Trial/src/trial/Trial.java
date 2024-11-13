5d58ff2c31618cac64d2da7a82a19a675a15c61f
JohnLloydGonzales <163238062+JohnLloydGonzales@users.noreply.github.com>
2024-11-13 15:15:13 +0800

    Add files via upload

---
+++ Gonzales_Johnlloyd/Trial/src/trial/Trial.java
@@ -0,0 +1,19 @@
+package trial;
+
+public class Trial {
+
+    public static void main(String[] args) {
+        
+        String name = "John";
+        String bloodType = "O";
+        Float gpa = 1.76f;
+        int age = 18;
+        
+        System.out.println("Hi my name is " + name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("My GPA is " + gpa);
+        System.out.println("My blood type is " + bloodType);
+
+    }
+    
+}
